bdcc855a8bbbe9d2e02e29c41ad8b1ba30d84172
RigenKronaj <84721068+RigenKronaj@users.noreply.github.com>
2022-02-11 14:05:54 +0100

    Add files via upload

---
+++ Javascript Exercises/Exercise 1/Exercise1.js
@@ -0,0 +1,6 @@
+function sumOrTripleSum(a, b) {
+    if (a === b)
+        return a * 3;
+    else
+        return a + b;
+}
---
+++ Javascript Exercises/Exercise 10/script.js
@@ -0,0 +1,12 @@
+function confirmEnding(str, target)
+{
+    if(str.charAt(str.length - 1) == target)
+        return true;
+    else
+        return false; 
+}
+
+let stringOne = "test";
+let stringTwo = "test";
+let targetOne = "d";
+let targetTwo = "t";
